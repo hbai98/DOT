de5d4f672d192d33ff0e0d1e393144cb30caf4c2
164140757 <164140757@qq.com>
2022-05-02 15:57:11 +0800

    mmHuman3D init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='DigitHuman',
+    version='0.0.1',
+    author='Haotian Bai',
+    author_email='haotianwhite@outlook.com',
+    description='Simple framework for 3D digital human reconstruction',
+    url='https://github.com/164140757/DigitHuman',
+    packages=setuptools.find_packages(),
+    license='Internal',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Internal',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ]
+)
